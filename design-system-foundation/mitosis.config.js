69818fa33e9b69cd00eae9cee727c7a994a98de4
Manu Ustenko <ustenko.mi@gmail.com>
2022-10-11 15:36:37 +0200

    Add mitosis

---
+++ design-system-foundation/mitosis.config.js
@@ -0,0 +1,7 @@
+// Configuration: https://github.com/BuilderIO/mitosis/blob/main/docs/configuration.md#typescript-configuration
+// Targets list: https://github.com/BuilderIO/mitosis/blob/main/packages/core/src/targets.ts
+
+module.exports = {
+  files: 'src/**',
+  targets: ['vue2', 'vue3', 'webcomponent', 'svelte', 'react'],
+};
